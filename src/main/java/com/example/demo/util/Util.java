1ca1d1b75d96795a802fa46138be8850ac50b7be
YouseefNoaman <youseefnoaman@gmail.com>
2021-08-11 22:01:37 +0200

    - Added ErrorDetail table to log all the errors in database - Added AOP logging

--- src/main/java/com/example/demo/util/Util.java
+++ src/main/java/com/example/demo/util/Util.java
@@ -9,7 +9,6 @@ public class Util {
 	 * @return boolean, if the String is not null and not empty  
 	 */
 	public boolean checkString(String s) {
-		System.err.println("s: " + s);
 		if (s != null && !s.trim().equals(""))
 			return true;
 		return false;
@@ -20,7 +19,6 @@ public class Util {
 	 * @return boolean, if the Customer is not null, and name and phoneNumber are not empty or null 
 	 */
 	public boolean checkCustomerObj(Customer c) {
-		System.out.println("c: " + c.toString());
 		if (c != null && checkString(c.getName()) && checkString(c.getPhoneNumber()))
 			return true;
 		return false;
